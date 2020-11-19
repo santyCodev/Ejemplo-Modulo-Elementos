2e157d6b2c7dba9bd2eda89338fc02034a31728b
santy <santi.codev@gmail.com>
2020-11-19 17:52:27 +0100

    Modulo elementos y componente dado

---
+++ EjemploModuloElementos/src/app/modules/elementos/elementos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DadoComponent } from './dado/dado.component';
+
+
+
+@NgModule({
+  declarations: [DadoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ElementosModule { }
